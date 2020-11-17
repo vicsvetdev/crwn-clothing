baf7de797d161ce1321b917fc9a539936b30ebbe
Victor Svetuskin <vicsvetdev@gmail.com>
2020-11-18 01:23:39 +0200

    broken homepage into smaller components, finished styling menu-item component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './pages/homepage/homepage.component.jsx';
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
