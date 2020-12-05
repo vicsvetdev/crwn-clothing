5a2e44b8df17fe48983c50691a209f085567e795
Victor Svetuskin <vicsvetdev@gmail.com>
2020-12-05 17:55:27 +0200

    updated stripe integration, and updated favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51Hv2UnCOsiNT8ScPttRMec1dBaboj2mMw7LiHlRP6hWUAXaAK74hJEe4zwjXcnUSw4jH2K5iiz0zWXxnVnBuB8d200xg54AjAd';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
